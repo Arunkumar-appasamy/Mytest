0e901cfe7ea2f14da4da1b0f34060df9ee4249a8
helen-appasamy <helenjoyce.a@appasamy.com>
2021-07-14 17:22:22 +0530

    Code correction

--- user_access_report/models/user_access.py
+++ user_access_report/models/user_access.py
@@ -1,3 +1,5 @@
+# -- coding: utf-8
+
 from odoo import api, fields, models
 from odoo import tools
 
@@ -6,31 +8,30 @@ class UserAccessReport(models.Model):
     _auto = False
     _description = "User Access Report Details"
 
-    login_id = fields.Char(comodel_name='res.users', string='Login', required=False, readonly=True)
-    user_name = fields.Many2one(comodel_name='res.users', string='Name', required=False, readonly=True)
-    user_department = fields.Many2one(comodel_name='hr.department', string='Department', required=False, readonly=True)
-    work_address = fields.Many2one(comodel_name='res.partner', string='Company', required=False, readonly=True)
-    application_name = fields.Many2one(comodel_name='ir.module.category', string='Menu', required=False, readonly=True)
-    access_name = fields.Char(comodel_name='res.groups', string='Access Right', required=False, readonly=True)
+    login_id = fields.Char(string='Login')
+    user_name_id = fields.Many2one('res.users', string='Name')
+    hr_department_id = fields.Many2one('hr.department', string='Department')
+    work_address_id = fields.Many2one('res.partner', string='Work Address')
+    category_id = fields.Many2one('ir.module.category', string='Menu')
+    access_id = fields.Char(string='Access Right')
 
 
-    _depends = {'hr.employee': ['department_id', 'address_id','user_id'], 'res.groups': ['category_id', 'name'],}
-
-    @api.model
     def init(self):
         tools.drop_view_if_exists(self._cr, 'user_access_view')
         self._cr.execute("""
             CREATE OR REPLACE VIEW user_access_view AS (
                 SELECT
                     row_number() over() as id,
-                    resuser.id as user_name,
-                    resuser.login as login_id,
-                    emp.department_id as user_department,
-                    emp.address_id as work_address,
-                    ref1.category_id as application_name,
-                    ref1.name as access_name
-                FROM res_users as resuser LEFT JOIN hr_employee as emp ON emp.user_id=resuser.id AND resuser.active=true
-                LEFT JOIN(
-                    SELECT res_groups.name,res_groups_users_rel.uid,res_groups.category_id
-                    FROM res_groups LEFT JOIN res_groups_users_rel ON res_groups.id=res_groups_users_rel.gid ) as ref1 ON resuser.id=ref1.uid AND resuser.active=true
-                )""")
+                    users.id as user_name_id,
+                    users.login as login_id,
+                    employee.department_id as hr_department_id,
+                    employee.address_id as work_address_id,
+                    temp_table.category_id as category_id,
+                    temp_table.name as access_id
+                FROM 
+                    res_users as users 
+                LEFT JOIN
+                    hr_employee as employee ON employee.user_id=users.id AND users.active=true
+                LEFT JOIN
+                    (SELECT res_groups.name,res_groups_users_rel.uid,res_groups.category_id
+                    FROM res_groups LEFT JOIN res_groups_users_rel ON res_groups.id=res_groups_users_rel.gid ) as temp_table ON users.id=temp_table.uid AND users.active=true)""")
